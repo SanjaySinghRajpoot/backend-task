05f700b7ee8c5e0ff15e2a20109ff6738b0e3d44
SanjaySinghRajpoot <iamsanjayrajpoot@gmail.com>
2022-09-01 13:49:21 +0530

    validate middleware

---
+++ middleware/validate.js
@@ -0,0 +1,19 @@
+// to validate the data enteted by the user
+export const validateData = (req, res, next) => {
+    const { income, savings, mobile } = req.body;
+  
+    if (income < savings) {
+      console.error("Invalid income: " + income + " and savings: " + savings);
+      next(new Error(`Invalid income: ${income} and savings: ${savings}`));
+      return;
+    }
+    if (isNaN(mobile)) {
+      next(new Error("Not a mobile number: " + mobile));
+      return;
+    }
+    if (mobile.length !== 10) {
+      next(new Error("Not a mobile number: " + mobile));
+      return;
+    }
+    next();
+  }
